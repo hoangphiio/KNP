1b10d39c2cf35b3aab781e7a073fcda0499b7309
Nguyễn Võ Hoàng Phi <phinvh@vuthao.local>
2023-04-19 13:05:30 +0700

    upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import aosModule from "./modules/aosModule.js";
+import swiperModule from "./modules/swiperModule.js";
+import dropdownModule from "./modules/dropdownModule.js";
+import tablistModule from "./modules/tablistModule.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  aosModule();
+  swiperModule();
+  dropdownModule();
+  tablistModule();
+});
