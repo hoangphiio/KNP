aecd505fab344b9cc401455ac31408338c6eb509
Nguyễn Võ Hoàng Phi <phinvh@vuthao.local>
2023-05-19 15:08:17 +0700

    fix

--- js/modules/swiperModule.js
+++ js/modules/swiperModule.js
@@ -1,31 +1,4 @@
 export default function swiperModule() {
-  const swiperCard = new Swiper(".customer-card .swiper", {
-    slidesPerView: 3,
-    spaceBetween: 40,
-    loop: true,
-    observer: true,
-    observeParents: true,
-    loopedSlides: 4,
-    speed: 1800,
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: true,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      500: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1160: {
-        slidesPerView: 3,
-      },
-    },
-  });
   const swiperTablist = new Swiper(".tablist-inner-panel-swipper .swiper", {
     loop: true,
     observer: true,
@@ -50,10 +23,10 @@ export default function swiperModule() {
       fadeEffect: {
         crossFade: true,
       }, 
-      autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-      },
+      // autoplay: {
+      //   delay: 2000,
+      //   disableOnInteraction: false,
+      // },
       pagination: {
         el: ".banner-pagination",
         clickable: true,
